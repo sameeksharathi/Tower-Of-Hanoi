51c13884c640ba07058513cfa702b56a465e54a9
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 20:14:24 +0530

    created

---
+++ TowerOfHanoiMain.java
@@ -0,0 +1,6 @@
+class TowerOfHanoiMain{
+    public static void main(String[] args) {
+        TowerOfHanoi obj = new TowerOfHanoi();
+        obj.m1(3,"Source","Auxilliary","Destination");
+    }
+}
