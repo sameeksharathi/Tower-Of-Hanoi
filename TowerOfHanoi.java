e0441bf6fac1c58b49449becc3a34ef70f6218f7
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 20:10:29 +0530

    Created

---
+++ TowerOfHanoi.java
@@ -0,0 +1,15 @@
+public class TowerOfHanoi {
+    public static void m1(int disk,String s,String a , String d){
+        if(disk==1){
+            System.out.println("Disk_"+disk+" FROM "+ s +" TO "+d);
+        }else{
+            m1(disk-1,s,d,a);
+            System.out.println("Disk_"+disk+ " FROM "+s+" TO "+d);
+            m1(disk-1,a,s,d);
+
+        }
+    }
+    public static void main(String[] args) {
+        m1(3,"Source","Auxilliary","Destination");
+    }
+}
